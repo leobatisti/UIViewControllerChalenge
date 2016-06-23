6d944d861dfad69c09ed78d99727d8ad578f2087
Leonardo Batisti de Faria <leobatisti@Roots-Mac.local>
2016-06-23 10:30:00 -0300

    solved chalenge

---
+++ UITableViewControllerChalenge/CCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTableViewController.h
+//  UITableViewControllerChalenge
+//
+//  Created by Leonardo Batisti de Faria on 23/06/16.
+//  Copyright © 2016 Leonardo Batisti de Faria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@end
